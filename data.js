ff8af370166a51b6e9dec58ec942617db36ee6d8
DESKTOP-L7QRVU0\utpal <hiren.patel@psav.com>
2019-01-29 13:02:23 +0530

    Corrected Formatting

--- data.js
+++ data.js
@@ -1,42 +1,47 @@
-
-var request = require('request');
+var request = require("request");
 
 module.exports = {
-	getToDos: function(callback) {
-		var url = 'https://jsonplaceholder.typicode.com/todos';
-		request.get({
-		    url: url,
-		    json: true,
-		    headers: {'User-Agent': 'request'}
-		  }, (err, res, data) => {
-		    if (err) {
-		      console.log('Error:', err);
-		      return null;
-		    } else if (res.statusCode !== 200) {
-		      console.log('Status:', res.statusCode);
-		      return null;
-		    } else {
-		      callback(data);
-		    }
-		});
-	},
+  getToDos: function(callback) {
+    var url = "https://jsonplaceholder.typicode.com/todos";
+    request.get(
+      {
+        url: url,
+        json: true,
+        headers: { "User-Agent": "request" }
+      },
+      (err, res, data) => {
+        if (err) {
+          console.log("Error:", err);
+          return null;
+        } else if (res.statusCode !== 200) {
+          console.log("Status:", res.statusCode);
+          return null;
+        } else {
+          callback(data);
+        }
+      }
+    );
+  },
 
-	getUserName: function(userId, callback) {
-		var url = 'https://jsonplaceholder.typicode.com/users/'+userId;
-		request.get({
-		    url: url,
-		    json: true,
-		    headers: {'User-Agent': 'request'}
-		  }, (err, res, data) => {
-		    if (err) {
-		      console.log('Error:', err);
-		      return null;
-		    } else if (res.statusCode !== 200) {
-		      console.log('Status:', res.statusCode);
-		      return null;
-		    } else {
-		      callback(data.username);
-		    }
-		});
-	}
-}
+  getUserName: function(userId, callback) {
+    var url = "https://jsonplaceholder.typicode.com/users/" + userId;
+    request.get(
+      {
+        url: url,
+        json: true,
+        headers: { "User-Agent": "request" }
+      },
+      (err, res, data) => {
+        if (err) {
+          console.log("Error:", err);
+          return null;
+        } else if (res.statusCode !== 200) {
+          console.log("Status:", res.statusCode);
+          return null;
+        } else {
+          callback(data.username);
+        }
+      }
+    );
+  }
+};
--- db.js
+++ db.js
@@ -1,48 +1,50 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
 client.connect();
 console.log("DB connection Initialized!");
 
-
 module.exports = {
-	cleanTable: function() {
-	  	var query = "DROP TABLE IF EXISTS todos";
-	  	client.query(query, (err, res) => {
-	  		if(err) {
-				console.log(err);
-	  		} else {
-	  			console.log("Table todos cleaned");
-	  		}
-	  		
-	  	});
-	},
+  cleanTable: function() {
+    var query = "DROP TABLE IF EXISTS todos";
+    client.query(query, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Table todos cleaned");
+      }
+    });
+  },
 
-	createTable: function() {
-		var query = "CREATE TABLE todos (id SERIAL, json_data TEXT, todo_id VARCHAR (50) )";
-		client.query(query,(err,res) => {
-			if(err) {
-				console.log(err);
-	  		} else {
-	  			console.log("Table todos created");
-	  		}
-				
-		});
-	},
+  createTable: function() {
+    var query =
+      "CREATE TABLE todos (id SERIAL, json_data TEXT, todo_id VARCHAR (50) )";
+    client.query(query, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Table todos created");
+      }
+    });
+  },
 
-	insertToDos: function(jsonData, todoId) {
-		var query = "INSERT INTO todos (json_data, todo_id) VALUES ('"+jsonData+"', '"+todoId+"' )";
-		client.query(query,(err,res) => {
-			if(err) {
-				console.log(err);
-	  		} else {
-	  			console.log("Todo id:"+todoId+" inserted to db");
-	  		}
-				
-		});
-	}
-}
+  insertToDos: function(jsonData, todoId) {
+    var query =
+      "INSERT INTO todos (json_data, todo_id) VALUES ('" +
+      jsonData +
+      "', '" +
+      todoId +
+      "' )";
+    client.query(query, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Todo id:" + todoId + " inserted to db");
+      }
+    });
+  }
+};
